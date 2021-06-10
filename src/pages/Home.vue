7a3f50af13e1ead08623b6c3141b244d599ff27e
ArthurSuz <931446361@qq.com>
2021-06-10 14:56:39 +0800

    init

---
+++ src/pages/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div :data-test="test">{{ test }}</div>
+</template>
+
+<script setup>
+import { computed } from "vue";
+const test = computed(() => {
+  let data = Math.random();
+  console.log("data", data);
+  return data;
+});
+</script>
